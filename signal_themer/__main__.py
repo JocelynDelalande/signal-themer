1dad2cd79e70108b7a24d6bda6e3e336bf03a3e7
CalfMoon <kritagyabhattarai@proton.me>
2024-05-01 10:29:13 +0545

    feat: define temp directory returning function

--- signal_themer/__main__.py
+++ signal_themer/__main__.py
@@ -9,3 +9,11 @@ def app_path():
             return "/usr/lib/signal-desktop/resources/app.asar"
         case "Windows":
             return os.path.join(os.getenv("LOCALAPPDATA"), r"Programs\signal-desktop\resources\app.asar")
+
+# returns temp directory depending on os in order to temporily store extracted asar
+def temp_path():
+    match platform.system():
+        case "Linux":
+            return "/temp/signal-themer/"
+        case "Windows":
+            return os.path.join(os.getenv("LOCALAPPDATA"), r"temp\signal-themer")
