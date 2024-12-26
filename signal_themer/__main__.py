4afbf9e77e57de20b846b8f267e8f31f949449fd
JocelynDelalande <JocelynDelalande@users.noreply.github.com>
2024-12-26 16:54:48 +0100

    Add support for .deb version of signal-desktop

--- signal_themer/__main__.py
+++ signal_themer/__main__.py
@@ -24,18 +24,31 @@ def theme_injector():
 
     shutil.rmtree(temp)
 
+APP_PATHS = {
+    "Linux": [
+        "/usr/lib/signal-desktop/resources/app.asar",
+        "/opt/Signal/resources/app.asar",  # Installation via .deb pkg
+    ],
+    "Windows": [os.getenv("LOCALAPPDATA", ""), r"Programs\signal-desktop\resources\app.asar"],
+}
+
 # returns path of signal-desktop
 def app_path():
-    match platform.system():
-        case "Linux":
-            return "/usr/lib/signal-desktop/resources/app.asar"
-        case "Windows":
-            return os.path.join(os.getenv("LOCALAPPDATA"), r"Programs\signal-desktop\resources\app.asar")
+    try:
+        paths = APP_PATHS[platform.system()]
+        for path in paths:
+            if os.path.isfile(path):
+                return path
+    except KeyError:
+        raise OSError(f"Unknown platform {platform.system()}")
+    else:
+        raise OSError(f"Is Signal installed ? looked for it in {paths}")
 
 # returns temp directory depending on os in order to temporily store extracted asar
 def temp_path():
     match platform.system():
         case "Linux":
+            
             return "/temp/signal-themer/"
         case "Windows":
             return os.path.join(os.getenv("LOCALAPPDATA"), r"temp\signal-themer")
